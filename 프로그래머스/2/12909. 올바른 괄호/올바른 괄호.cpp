2ed81f8d18e7552fc6f9bcaba3e9608070e163fe
khy0e <dhy04029@gmail.com>
2024-09-04 09:14:10 +0900

    [level 2] Title: 올바른 괄호, Time: 0.54 ms, Memory: 3.82 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+#include <stack>
+using namespace std;
+
+bool solution(string s)
+{
+	stack<char> st;
+	for (int i = 0; i < s.length();i++) {
+		if (s[i] == '(')
+			st.push(s[i]);
+		else {
+			if (st.empty())
+				return false;
+			else
+				st.pop();
+		}
+	}
+	if (st.empty())
+		return true;
+    else
+		return false;
+}
