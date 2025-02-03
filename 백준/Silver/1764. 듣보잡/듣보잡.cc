2dada3e76bb7443d053ea4e46df241cbfb5b9cde
khy0e <dhy04029@gmail.com>
2025-02-03 15:48:02 +0900

    [Silver IV] Title: 듣보잡, Time: 76 ms, Memory: 12984 KB -BaekjoonHub

--- 백준/Silver/1764. 듣보잡/듣보잡.cc
+++ 백준/Silver/1764. 듣보잡/듣보잡.cc
@@ -1,36 +1,32 @@
 #include <iostream>
 #include <map>
 #include <string>
-#include <vector>
 using namespace std;
 
 int main() {
-	int i, j,num=0;
-	string name;
-	map <string, int> m;
-	vector<string> v;
-	cin >> i;
-	cin >> j;
 
-	for (int k = 0; k < i; k++) {
-		cin >> name;
-		m[name]++;
-	}
-	for (int k = 0; k < j; k++) {
-		cin >> name;
-		m[name]++;
-	}
-	for (auto it = m.begin(); it != m.end(); it++) {
-		if ((it->second) == 2) {
-			num++;
-			v.push_back(it->first);
-		}
-	}
-	cout << num << '\n';
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
 
-	// 중복된 이름 출력
-	for (const auto& name : v) {
-		cout << name << '\n';
-	}
-	return 0;
+    int n1, n2, num=0;
+    string name;
+    map<string, int> m;
+
+    cin >> n1 >> n2;
+    
+    for (int i = 0; i < n1+n2; i++) {
+        cin >> name;
+        m[name]++;
+        if (m[name] == 2) {
+            num++;
+        }
+    }
+    cout << num << '\n';
+
+    for (auto it : m) {
+        if (it.second == 2) {
+            cout << it.first << '\n';
+        }
+    }
+    return 0;
 }
